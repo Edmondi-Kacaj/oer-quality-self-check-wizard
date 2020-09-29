a6fe8d10e275d87825834799fed39ac17e510d47
Hunar Karim <hunar.karim@tib.eu>
2020-09-29 15:07:27 +0200

    new page

---
+++ wizard/html/step_2.js
@@ -0,0 +1,46 @@
+var step_2 = `<div class="row" id="step-2">
+    <div class="container">
+       <div  class="row">
+          <div class="col-12 side-content">
+             <div  class="bs-vertical-wizard">
+                <ul>
+                   <li class="current">
+                      <p class="pwizard">100%</p>
+                      <a href="#"><span onClick='queryById(3,this.id)' class="ptext">Die Bildungsressource beinhaltet oder besteht aus klar abgrenzbaren Lehr- bzw. Lernobjekten, die problemlos in Kursen 
+                      oder anderen Kontexten (z. B. in einem Studiengang) eingesetzt werden können. Die Inhalte sind in sich geschlossen und 
+                      setzen nicht die Verwendung anderer Materialien voraus</span>
+                      </a>
+                   </li>
+                   <li class="current">
+                      <p class="pwizard">75%</p>
+                      <a href="#">
+                         <p onClick='queryById(3,this.id)' class="ptext1"></p>
+                      </a>
+                   </li>
+                   <li class="current">
+                      <p class="pwizard">50%</p>
+                      <a href="#"><span onClick='queryById(3,this.id)' class="ptext">Die Bildungsressource beinhaltet oder besteht aus klar abgrenzbaren Lehr- bzw. Lernobjekten, die problemlos in Kursen 
+                      oder anderen Kontexten (z. B. in einem Studiengang) eingesetzt werden können. Die Inhalte sind in sich geschlossen und 
+                      setzen nicht die Verwendung anderer Materialien voraus</span>
+                      </a>
+                   </li>
+                   <li class="current">
+                      <p class="pwizard">25%</p>
+                      <a href="#">
+                         <span class="desc">
+                            <p onClick='queryById(3,this.id)' class="ptext1"></p>
+                         </span>
+                      </a>
+                   </li>
+                   <li class="current">
+                      <p class="pwizard"> 0%</p>
+                      <a href="#"><span onClick='queryById(3,this.id)' class="ptext">Aufgrund von inhaltlicher Abhängigkeit ist es nicht möglich, Lehr- bzw. Lernobjekte der Bildungsressource in anderen 
+                      Kontexten wiederzuverwenden</span>
+                      </a>
+                   </li>
+                </ul>
+             </div>
+          </div>
+       </div>
+    </div>
+ </div>`
